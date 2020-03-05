38104149ab6e3320b64dcfdc1d69cb30ee89a6fa
Isaac Ñuflo Gamarra <inuflo@novatronic.com>
2020-03-05 17:07:50 -0500

    HQL

--- src/com/luv2code/hibernate/demo/FetchJoinDemo.java
+++ src/com/luv2code/hibernate/demo/FetchJoinDemo.java
@@ -3,12 +3,13 @@ package com.luv2code.hibernate.demo;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import com.luv2code.hibernate.entity.Course;
 import com.luv2code.hibernate.entity.Instructor;
 import com.luv2code.hibernate.entity.InstructorDetail;
 
-public class EagerLazyDemo {
+public class FetchJoinDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -25,13 +26,22 @@ public class EagerLazyDemo {
 			
 			session.beginTransaction();
 			
-			Instructor tempInstructor = session.get(Instructor.class, 4);
+			int theId = 4;
+			
+			Query<Instructor> query = 
+					session.createQuery("select i from Instructor i "
+							+ "JOIN FETCH i.courses "
+							+ "where i.id=:theInstructorId", 
+							Instructor.class);
+			query.setParameter("theInstructorId", theId);
+			
+			Instructor tempInstructor = query.getSingleResult();
 			
 			System.out.println("luv2code: Instructor: " + tempInstructor);
 			
-			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
-			
 			session.getTransaction().commit();
+			
+			System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
 			
 			System.out.println("luv2code: done");
 		} catch (Exception e) {

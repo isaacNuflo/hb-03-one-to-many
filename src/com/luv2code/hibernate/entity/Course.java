732c245d66caa5958a15e6d12de7f2fa11531769
isaacNuflo <isaac.nuflo21@gmail.com>
2020-03-07 08:44:42 -0500

    one to many relationship

--- src/com/luv2code/hibernate/entity/Course.java
+++ src/com/luv2code/hibernate/entity/Course.java
@@ -1,13 +1,18 @@
 package com.luv2code.hibernate.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,17 +22,20 @@ public class Course {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
-	
+
 	@Column(name = "title")
 	private String title;
-	
-	@ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, 
-			CascadeType.REFRESH})
+
+	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
-	
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
+
 	public Course() {
-		
+
 	}
 
 	public Course(String title) {
@@ -58,10 +66,24 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public void addReview(Review review) {
+		if (reviews == null) {
+			reviews = new ArrayList<Review>();
+		}
+		reviews.add(review);
+	}
+
 	@Override
 	public String toString() {
-		return "Course [id=" + id + ", title=" + title + "]";
+		return "Course [id=" + id + ", title=" + title + ", reviews=" + reviews + "]";
 	}
-	
-	
+
 }
